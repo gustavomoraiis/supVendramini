88b5dffc0ede39ee91a1becdfa4b727d5904ac1b
gustavoxdhenrique <gustavohenriquecfal@gmail.com>
2020-11-03 08:06:23 -0300

    textos modal produtos

--- js/cart.js
+++ js/cart.js
@@ -161,13 +161,13 @@ var shoppingCart = (function() {
     for(var i in cartArray) {
       output += "<tr>"
         + "<td>" + cartArray[i].name + "</td>" 
-        + "<td>(" + cartArray[i].price + ")</td>"
+        + "<td>(R$ " + cartArray[i].price + ")</td>"
         + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
         + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
         + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
         + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
         + " = " 
-        + "<td>" + cartArray[i].total + "</td>" 
+        + "<td>R$ " + cartArray[i].total + "</td>" 
         +  "</tr>";
     }
     $('.show-cart').html(output);
